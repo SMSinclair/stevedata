1d2849265a32d771be9256c0c086bb5e68141686
SMSinclair <SMSinclair@gmail.com>
2019-07-11 21:16:47 -0400

    Changed directory name

---
+++ stevedata_smsinclair/__init__.py
@@ -0,0 +1,11 @@
+```
+stevedata - a collection of data science helper functions for steve
+```
+
+import pandas as pd
+import numpy as np
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
+
+
