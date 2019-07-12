f505fc4befb9267eedb82394ce6bca4a3f370d2f
SMSinclair <SMSinclair@gmail.com>
2019-07-11 22:11:26 -0400

    changed license, please work

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     'https://github.com/SMSinclair/stevedata',
     packages=setuptools.find_packages(),
     classifiers=['Programming Language :: Python :: 3',
-                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                 'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
                  'Operating System :: OS Independent'],
 )
 
