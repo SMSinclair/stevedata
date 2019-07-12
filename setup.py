cfe27c043d9c26ff62c4cce2e94e139dc3ee0606
SMSinclair <SMSinclair@gmail.com>
2019-07-11 22:06:42 -0400

    """mayybe fixed it"""

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-""" stevedata - a collection of Data Science helper functions
-"""
-
 import setuptools
 
 REQUIRED = [
@@ -8,25 +5,22 @@ REQUIRED = [
     "pandas"
 ]
 
-with open("README.md", "r") as fh:
-    LONG_DESCRIPTION = fh.read()
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setuptools.setup(
-    name="stevedata-smsinclair",
-    version="0.0.1",
-    author="SMSinclair",
-    author_email="smsinclair@gmail.com",
-    description="Basic DataFrame tools. Split dates or report nulls.",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/SMSinclair/stevedata",
+    name='stevedata-smsinclair',
+    version='0.0.1',
+    author='SMSinclair@gmail.com',
+    author_email='smsinclair@gmail.com',
+    description='Some very basic DataFrame tools.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=
+    'https://github.com/SMSinclair/stevedata',
     packages=setuptools.find_packages(),
-    python_requires=">=3.5",
-    install_requires=REQUIRED,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",   
-        "Operating System :: OS Independent",
-    ],
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                 'Operating System :: OS Independent'],
 )
 
